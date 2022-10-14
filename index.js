e6304021970111985b818f132e39006f04e1acca
FemiAdesola <75606768+FemiAdesola@users.noreply.github.com>
2022-10-14 17:43:54 +0300

    creates json file for question and fecth methdo

--- index.js
+++ index.js
@@ -15,39 +15,20 @@ let point = 0;
 let questionCounter = 0;
 let availableQuesions = [];
 
-let questions = [
-    {
-        question: 'How can you attempt to access the property `a.b` on `obj` without throwing an error if a is undefined? let obj = {};',
-        choice1: 'obj?.a.b',
-        choice2: 'obj.a?.b',
-        choice3: 'obj[a][b]',
-        choice4: 'obj.?a.?b',
-        answer: 2,
-    },
-    {
-        question:
-            `What happens when you run this code?
-            if (true) { var x = 5;
-            const y = 6;
-            let z = 7; }
-            console.log(x + y + z);`
-        ,
-        choice1: "It will throw a ReferenceError about x",
-        choice2: "It will print 18",
-        choice3: "It will print undefined",
-        choice4: "It will throw a ReferenceError about y",
-        answer: 4,
-    },
-    {
-        question: " How do you write 'Hello World' in an alert box?",
-        choice1: "msgBox('Hello World');",
-        choice2: "alertBox('Hello World');",
-        choice3: "msg('Hello World');",
-        choice4: "alert('Hello World');",
-        answer: 4,
-    },
-];
+let questions = [];
 
+// fetch method
+fetch("questions.json")
+    .then((res) => {
+        return res.json();
+    })
+    .then((getQuestions) => {
+        questions = getQuestions;
+        startQuiz();
+    })
+    .catch((error) => {
+        console.error(error);
+    });
 
 // point
 const CORRECT_BONUS = 10;
@@ -130,7 +111,7 @@ const incrementScore = num => {
 };
 
 
-startQuiz()
+// startQuiz()
 
 
    // const classToApply = "incorrect";
