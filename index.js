b76d1f3415cdbe2fe50ff51f1b88a30d0af73c67
FemiAdesola <75606768+FemiAdesola@users.noreply.github.com>
2022-10-14 12:35:51 +0300

    creates index.js functionality for the question and starting quiz

---
+++ index.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("option-text"));
+
+let currentQuestion = {};
+let acceptingAnswers = false;
+let point = 0;
+let questionCounter = 0;
+let availableQuesions = [];
+
+let questions = [
+    {
+        question: 'How can you attempt to access the property `a.b` on `obj` without throwing an error if a is undefined? let obj = {};',
+        choice1: 'obj?.a.b',
+        choice2: 'obj.a?.b',
+        choice3: 'obj[a][b]',
+        choice4: 'obj.?a.?b',
+        answer: 2,
+    },
+    {
+        question:
+            `What happens when you run this code?
+            if (true) { var x = 5;
+            const y = 6;
+            let z = 7; }
+            console.log(x + y + z);`
+        ,
+        choice1: "It will throw a ReferenceError about x",
+        choice2: "It will print 18",
+        choice3: "It will print undefined",
+        choice4: "It will throw a ReferenceError about y",
+        answer: 4,
+    },
+    {
+        question: " How do you write 'Hello World' in an alert box?",
+        choice1: "msgBox('Hello World');",
+        choice2: "alertBox('Hello World');",
+        choice3: "msg('Hello World');",
+        choice4: "alert('Hello World');",
+        answer: 4,
+    },
+];
+
+
+// point
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 3;
+
+const startQuiz = () => {
+    questionCounter = 0;
+    point = 0;
+    availableQuesions = [...questions];
+   
+};
+
+startQuiz()
