a7db5d858b2ba8c5e0603af75a21fd6dc35884b3
FemiAdesola <75606768+FemiAdesola@users.noreply.github.com>
2022-10-14 16:38:40 +0300

    Added function in complete.js to store point in localStorage

--- complete.js
+++ complete.js
@@ -10,6 +10,25 @@ username.addEventListener('keyup', () => {
     savePointBtn.disabled = !username.value;
 });
 
+const topPoints = JSON.parse(localStorage.getItem('topPoint')) || [];
+
+const MAX_TOP_POINTS = 5;
+
+
 const saveTopPoint = (e) => {
     e.preventDefault();
+
+    // get top point in local storage 
+    const point = {
+        point: Math.floor(Math.random()*100),
+    //    point: RecentPoint,
+        name: username.value,
+    };
+    topPoints.push(point);
+   
+    topPoints.sort((a, b) => b.point - a.point); // For sorting from higher point to lower point 
+    topPoints.splice(5); // For stopping score at max of 5 people
+
+    localStorage.setItem('topPoint', JSON.stringify(topPoints));
+    window.location.assign('/');
 };
